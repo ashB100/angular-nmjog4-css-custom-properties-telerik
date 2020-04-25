6a8d343eb5ae2022ade75ad09a977d8e6218d48c
ashB100 <ashnita01@gmail.com>
2020-04-25 19:54:12 +0100

    3 files modified

--- src/app/hsla-with-custom-property-binding/hsla-hostbinding/hsla-hostbinding.component.ts
+++ src/app/hsla-with-custom-property-binding/hsla-hostbinding/hsla-hostbinding.component.ts
@@ -12,6 +12,8 @@ export class HslaHostbindingComponent  {
   lightness = new FormControl(40);
   opacity = new FormControl(1);
 
+  mode = new FormControl('multiply');
+  blendMode = ['normal', 'multiply', 'screen', 'overlay', 'darken', 'lighten', 'color-dodge', 'color-burn', 'hard-light', 'soft-light', 'difference', 'exclusion', 'hue', 'saturation', 'color', 'luminosity'];
   @HostBinding('style.--user-selected-color') get hslaColor() {
     return `hsla(${this.hue.value}, ${this.saturation.value}%, ${this.lightness.value}%, ${this.opacity.value})`;
   }
